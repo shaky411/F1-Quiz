e68979c8d07085c6dc7d2e797e6c52f17d1125b3
Marc Norris <marcnorris007@icloud.com>
2023-01-12 10:16:38 +0000

    added an array of objects to store quiz questions

---
+++ script.js
@@ -0,0 +1,20 @@
+// Questions
+
+// increment this value
+let currentQuestion = 0; 
+
+
+// use event delegation
+// only need 1 event listeners
+// local storage needed for high score
+// Structure the DATA
+// Carasel activity
+
+let questions = [
+    {question: "Which driver has the most pole positions in F1 history?", answers: ["Sebastian Vettel", "Fernando Alonso", "Lewis Hamilton"], correctAnswer: 3},
+    {question: "What is the longest cicuit on the current F1 Calendar?", answers: ["Spa", "Silverstone", "Monza"], correctAnswer: 1},
+    {question: "Driver with the most wins in F1 history is?", answers: ["Max Verstappen", "Jenson Button", "Lewis Hamilton"], correctAnswer: 3},
+    {question: "The most successful team in F1 is?", answers: ["McLaren", "Ferrari", "Mercedes"], correctAnswer: 2},
+    {question: "Who is the youngest driver to win an F1 race", answers: ["Sebastian Vettel", "Charles Leclerc", "Max Verstappen"], correctAnswer: 3},
+    {question: "Driver with the most wins in an F1 season is?", answers: ["Sebastian Vettel", "Max Verstappen", "Lewis Hamilton"], correctAnswer: 2}
+]
