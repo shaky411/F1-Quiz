993d1db76fd55f9c8a37ad55decf127167e1dfe9
Marc Norris <marcnorris007@icloud.com>
2023-01-13 19:12:21 +0000

    created function to start the quiz

---
+++ example.js
@@ -0,0 +1,205 @@
+const timerEl = document.getElementById('timer');
+const questionEl = document.getElementById('question');
+const answerOne = document.getElementById('answer-1');
+const answerTwo = document.getElementById('answer-2');
+const answerThree = document.getElementById('answer-3');
+const answerFour = document.getElementById('answer-4');
+const finalScoreEl = document.getElementById('final-score');
+const initialsEl = document.getElementById('initials');
+const highScoresListEl = document.getElementById('highscores-list')
+const shuffledQuestionBank = shuffleQB();
+
+let secondsLeft = 60;
+let currentScore = 0;
+let currentQ = -1;
+let finalScore;
+
+// Move to next div # from current div #
+function changeDiv(curr, next) {
+    document.getElementById(curr).classList.add('hide');
+    document.getElementById(next).removeAttribute('class')
+};
+
+// Handle Start button click
+function startGame() {
+    changeDiv('start-page', 'question-container');
+    nextQuestion();
+    startTimer();
+};
+
+// Timer function
+function startTimer() {
+    timerEl.textContent = secondsLeft;
+    let timerInterval = setInterval(
+        () => {
+            secondsLeft--;
+            timerEl.textContent = secondsLeft;
+            if (secondsLeft <= 0) {
+                clearInterval(timerInterval);
+                endGame();
+            }
+        }, 1000);
+};
+
+function nextQuestion() {
+    currentQ++;
+    // If there are no more questions, end the game
+    if (currentQ === shuffledQuestionBank.length) {
+        secondsLeft = 0;
+        endGame();
+    } else {
+        // Otherwise populate questionEl
+        questionEl.textContent = shuffledQuestionBank[currentQ].question;
+        // and populate answer buttons
+        let arr = [answerOne, answerTwo, answerThree, answerFour];
+        let i = 0;
+        arr.forEach(element => {
+            element.textContent = shuffledQuestionBank[currentQ].answersArray[i].answer;
+            i++
+        }, i);
+    };
+};
+
+// When user clicks an answer button
+function handleAnswerClick(event) {
+    // Get the correct answer string
+    let correctAnswer = getCorrectAnswer(currentQ);
+    // Compare to user click
+    if (event.target.textContent === correctAnswer) {
+        currentScore += 10;
+        // color indicates correct choice
+        event.target.classList.add('correct')
+    } else {
+        secondsLeft -= 10;
+        // color indicates wrong choice
+        event.target.classList.add('wrong')
+    }
+    // Wait 0.5 sec, reset btn color, go to next question
+    setTimeout(
+        () => {
+            event.target.className = 'btn';
+            nextQuestion();
+        }, 500);
+};
+
+function getCorrectAnswer(currentQ) {
+    let arr = shuffledQuestionBank[currentQ].answersArray;
+    // loop through answersArray, identify correct answer
+    for (let j = 0; j < arr.length; j++) {
+        if (arr[j].correct) {
+            // return correct answer.
+            return arr[j].answer
+        }
+    }
+};
+
+function endGame() {
+    timerEl.textContent = 0;
+    changeDiv('question-container', 'results-page');
+    // Log currentScore on results page
+    finalScore = currentScore;
+    finalScoreEl.textContent = finalScore;
+}
+
+function handleSubmit() {
+    let initials = initialsEl.value;
+    // get array from storage, or initialize as empty array
+    let highScoresList = JSON.parse(localStorage.getItem('highScores')) || [];
+    // push new score to array
+    highScoresList.push({ initials: initials, score: finalScore });
+    // sort array ascending
+    highScoresList = highScoresList.sort((curr, next) => {
+        if (curr.score < next.score) {
+            return 1
+        } else if (curr.score > next.score) {
+            return -1
+        } else {
+            return 0
+        }
+    });
+    // set updated array to local storage
+    localStorage.setItem('highScores', JSON.stringify(highScoresList))
+    // go to highscores page
+    window.location.href = './highscores.html';
+}
+
+
+
+
+
+
+// Questions
+
+const questionBank = [
+
+    {
+        question: 'Which is NOT a primitive data type?',
+        answersArray: [
+            { answer: 'array', correct: true },  // Correct
+            { answer: 'string', correct: false },
+            { answer: 'boolean', correct: false },
+            { answer: 'undefined', correct: false }]
+    },
+
+    {// Updated 1/30/21
+        question: 'Which of these array methods returns an array?',
+        answersArray: [
+            { answer: 'Array.splice()', correct: true }, // Correct
+            { answer: 'Array.forEach()', correct: false },
+            { answer: 'Array.find()', correct: false },
+            { answer: 'Array.reduce()', correct: false }]
+    },
+
+    { // Updated 1/30/21
+        question: 'What is the function of `Array.unshift()`?',
+        answersArray: [
+            { answer: 'Removes first element of array', correct: true },  // Correct
+            { answer: 'Removes last element of array', correct: false },
+            { answer: 'Adds element to end of array', correct: false },
+            { answer: 'Adds element to start of array', correct: false }]
+    },
+
+    {
+        question: 'Which of these statements returns the number 2?',
+        answersArray: [
+            { answer: 'parseInt("2.9")', correct: true },  // Correct
+            { answer: 'Math.floor(1.9)', correct: false },
+            { answer: 'Math.round(2.9)', correct: false },
+            { answer: 'parseFloat("2.9")', correct: false }]
+    },
+
+    {
+        question: 'Which of the following is a comparison operator?',
+        answersArray: [
+            { answer: '!==', correct: true },  // Correct
+            { answer: '=', correct: false },
+            { answer: '+=', correct: false },
+            { answer: '--', correct: false }]
+    }
+
+];
+
+
+
+
+
+
+// High Scores
+
+function populateHighScores() {
+    // get array from storage, or initialize as empty array
+    let highScoresList = JSON.parse(localStorage.getItem('highScores')) || [];
+    // populate highscores list
+    let list = '';
+    highScoresList.forEach(score => {
+        list = list + '<p>' + score.initials + '  :  ' + score.score + '</p>';
+    });
+    highScoresListEl.innerHTML = list;
+}
+
+function resetScores() {
+    localStorage.clear();
+    populateHighScores();
+}
+
+populateHighScores();
--- script.js
+++ script.js
@@ -1,20 +1,146 @@
+// Notes for my meeting tutor:
+// getElementByID
+// querySlector
+// CreateElement
+// appendChild
+// How do these enable me to allow the JS and HTML to communicate?
+
+// event keyword!
+
+const timerEl = document.getElementById("time");
+const answerOne = document.getElementById("choices");
+const answerTwo = document.getElementById("choices");
+const answerThree = document.getElementById("choices");
+
+// Decrement this value
+let count = 100;
+let incorrectCount = -10;
+let currentQ = 0;
+
+// const lastQuestion = questions.length -1;
+
+// When the start quiz button is clicked, count needs to start decreasing.
+// question 1 needs to be displayed.
+
 // Questions
+// This is an array of objects to store my questions, possible answers and correct answer.
+let questions = [
 
-// increment this value
-let currentQuestion = 0; 
+    {
+        question: "Which driver has the most pole positions in F1 history?",
+        choices: ["Sebastian Vettel", "Fernando Alonso", "Lewis Hamilton"],
+        answer: 3
+    },
+
+    {
+        question: "What is the longest cicuit on the current F1 Calendar?",
+        choices: ["Spa", "Silverstone", "Monza"],
+        answer: 1
+    },
+
+    {
+        question: "Driver with the most wins in F1 history is?",
+        choices: ["Max Verstappen", "Jenson Button", "Lewis Hamilton"],
+        answer: 3
+    },
+
+    {
+        question: "The most successful team in F1 is?",
+        choices: ["McLaren", "Ferrari", "Mercedes"],
+        answer: 2
+    },
+
+    {
+        question: "Who is the youngest driver to win an F1 race",
+        choices: ["Sebastian Vettel", "Charles Leclerc", "Max Verstappen"],
+        answer: 3
+    },
+
+    {
+        question: "Driver with the most wins in an F1 season is?",
+        choices: ["Sebastian Vettel", "Max Verstappen", "Lewis Hamilton"],
+        answer: 2
+    }
+
+];
+
+// Handle start button
+function startQuiz(event) {
+    event.preventDefault()
+    console.log("Start quiz trigerred")
+
+    // Switch from start screen to quiz
+    let startScreen = document.getElementById("start-screen")
+    startScreen.classList.add("hide")
+
+    // Show first question
+    let questionContainer = document.getElementById("questions")
+    questionContainer.classList.toggle("hide");
+
+    // startTimer();
+    renderQuestion()
+}
+
+// FUNCTIONS
+// function to start quiz - Display first question & possible answers
+// Function to handle next Question
+// Function to handle selected answer
+// function to get correct answer
+// function to end quiz
+// function to submit results
 
 
+// {question: "Which driver has the most pole positions in F1 history?", 
+// choices: ["Sebastian Vettel", "Fernando Alonso", "Lewis Hamilton"], 
+// answer: 3},
+// Show the questions to the user
+function renderQuestion() {
+    const currentQuestion = questions[currentQ].question
+    console.log(currentQuestion)
+
+    let questionTitle = document.getElementById("question-title");
+    questionTitle.textContent = currentQuestion
+
+    var choicesContainer = document.getElementById("choices")
+    for (let i = 0; i < questions[currentQ].choices.length; i++) {
+        var button = document.createElement("button")
+        console.log(button)
+        button.textContent = questions[currentQ].choices[i]
+        button.addEventListener("click", checkAnswer)
+        choicesContainer.append(button)
+    }
+}
+
+function checkAnswer(event){
+    event.preventDefault()
+    console.log(event)
+    console.log(event.target)
+    //did I answer correctly?
+   
+
+    //are you done with the quiz? If you aren't
+    //cuurentQ += 1
+    //renderQuestion()
+
+    //endQuiz()
+}
+
+
+
+let startBtn = document.getElementById("start");
+startBtn.addEventListener("click", startQuiz)
+
+// Step one - WHEN I click the start quiz THEN the first question appears
+// What needs to happen here?
+
+
+// Step two - WHEN the question appears THEN there is one question AND 4 possible answers
+// Step three - WHEN I click the answer THEN it checks if the answer is correct or incorrect
+// Step four - WHEN I click the correct answer THEN the score increases ...
+
+// Useful info:
 // use event delegation
 // only need 1 event listeners
 // local storage needed for high score
 // Structure the DATA
 // Carasel activity
-
-let questions = [
-    {question: "Which driver has the most pole positions in F1 history?", answers: ["Sebastian Vettel", "Fernando Alonso", "Lewis Hamilton"], correctAnswer: 3},
-    {question: "What is the longest cicuit on the current F1 Calendar?", answers: ["Spa", "Silverstone", "Monza"], correctAnswer: 1},
-    {question: "Driver with the most wins in F1 history is?", answers: ["Max Verstappen", "Jenson Button", "Lewis Hamilton"], correctAnswer: 3},
-    {question: "The most successful team in F1 is?", answers: ["McLaren", "Ferrari", "Mercedes"], correctAnswer: 2},
-    {question: "Who is the youngest driver to win an F1 race", answers: ["Sebastian Vettel", "Charles Leclerc", "Max Verstappen"], correctAnswer: 3},
-    {question: "Driver with the most wins in an F1 season is?", answers: ["Sebastian Vettel", "Max Verstappen", "Lewis Hamilton"], correctAnswer: 2}
-]
